7cbe6d3402c8f9b17a30655d82d4efc4b70c1a96
Victor Basso <victorbasso@sagaranatech.com>
2015-01-16 22:35:09 -0300

    plot chain

--- src/Plot.py
+++ src/Plot.py
@@ -23,51 +23,63 @@ def _slider_val(slider):
     return slider.val
 
 
+def _create_slider(param, i):
+    axes = plt.axes([left, bottom + i*slspace, width, height])
+    return Slider(axes, param['name'], param['min'], param['max'], valinit=_param_init(param))
+
+
 class Plot:
 
-    def __init__(self, tfun, kfun, params, tmin=0, tmax=1, tstep=0.001, ymin=-10, ymax=10):
+    def __init__(self, functions, tmin=0, tmax=1, tstep=0.001, ymin=-10, ymax=10):
         self.tmin = tmin
         self.tmax = tmax
         self.ymin = ymin
         self.ymax = ymax
         self.t = np.arange(tmin, tmax, tstep)
-        self.fun1 = tfun
-        self.fun2 = kfun
-        self.params = params
-        self.sliders = []
-
-    def _create_slider(self, param):
-        i = len(self.sliders)
-        axes = plt.axes([left, bottom + i*slspace, width, height])
-        slider = Slider(axes, param['name'], param['min'], param['max'], valinit=_param_init(self.params[i]))
-        self.sliders.append(slider)
+        self.functions = functions
 
     def _setup_sliders(self):
-        for p in self.params:
-            self._create_slider(p)
+        i = 0
+        for fun_dict in self.functions:
+            sliders = []
+            for param in fun_dict['params']:
+                i += 1
+                sliders.append(_create_slider(param, i))
+            fun_dict['sliders'] = sliders
 
         def update(val):
-            values = map(_slider_val, self.sliders)
-            x = self.fun1(self.t, *values)
-            X = self.fun2(x, *values)
-            self.tplot.set_ydata(x)
-            self.kplot.set_ydata(X)
-            self.fig.canvas.draw_idle()
 
-        for s in self.sliders:
-            s.on_changed(update)
+            x = self.t
+            for fun_dict in self.functions:
+                fun = fun_dict['fun']
+                plot = fun_dict['plot']
+                sliders = fun_dict['sliders']
+
+                values = map(_slider_val, sliders)
+                y = fun(x, *values)
+                plot.set_ydata(y)
+                self.fig.canvas.draw_idle()
+                x = y
+
+        for fun_dict in self.functions:
+            sliders = fun_dict['sliders']
+            for s in sliders:
+                s.on_changed(update)
+
 
     def show(self):
-        values = map(_param_init, self.params)
-        x = self.fun1(self.t, *values)
-        X = self.fun2(x, *values)
+        n = len(self.functions)
+        self.fig, ax = plt.subplots(n, sharex=True)
 
-        self.fig, (ax1, ax2) = plt.subplots(2, sharex=True)
-        self.tplot, = ax1.plot(self.t, x)
-        self.kplot, = ax2.plot(self.t, X)
-        ax1.axis([self.tmin, self.tmax, self.ymin, self.ymax])
-        ax2.axis([self.tmin, self.tmax, self.ymin, self.ymax])
+        x = self.t
+        for i, fun_dict in enumerate(self.functions):
+            fun = fun_dict['fun']
+            values = map(_param_init, fun_dict['params'])
+            y = fun(x, *values)
+            fun_dict['plot'], = ax[i].plot(x, y)
+            x = y
+            ax[i].axis([self.tmin, self.tmax, self.ymin, self.ymax])
+
         self._setup_sliders()
-
         plt.subplots_adjust(bottom=bottom + 2*height + slspace + margin)
         plt.show()
